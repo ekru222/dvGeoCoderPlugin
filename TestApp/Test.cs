b5f52b6375bc43cd682f6aca441c1b2d3df85c2c
Rutledge <erutledge@ios.doi.gov>
2025-07-16 14:54:40 -0600

    Added some stuff to prepare for testing

---
+++ TestApp/Test.cs
@@ -0,0 +1,29 @@
+﻿// YourPluginTests.cs - Your unit tests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Microsoft.Xrm.Sdk;
+
+[TestClass]
+public class craUserLocationTest
+{
+    [TestMethod]
+    public void craUserLocationTestPluginLogic()
+    {
+        // Arrange
+        var context = new Mock<IPluginExecutionContext>();
+        var serviceFactory = new Mock<IOrganizationServiceFactory>();
+        var service = new Mock<IOrganizationService>();
+        
+        // Setup mocks
+        context.Setup(c => c.InputParameters).Returns(new ParameterCollection());
+        serviceFactory.Setup(f => f.CreateOrganizationService(It.IsAny<Guid>()))
+                  .Returns(service.Object);
+        
+        // Act
+        var plugin = new craRTOEmployeeCreatePost();
+        // Test your plugin logic
+        
+        // Assert
+        // Verify expected behavior
+    }
+}
